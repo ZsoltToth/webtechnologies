27a3810596ed61809b9d06f0eb42c60330bff5e8
tothzs <tothzs@iit.uni-miskolc.hu>
2015-12-26 09:55:52 +0100

    model, dao, service, controller and web modules have been created.

---
+++ real-estate/real-estate-controller/src/main/java/hu/iit/uni/miskolc/iit/webtech/real/estate/controller/App.java
@@ -0,0 +1,13 @@
+package hu.iit.uni.miskolc.iit.webtech.real.estate.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
