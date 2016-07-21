59322a8ef041681d6522a2c29bd18380630c060e
Zsolt Toth <tothzs@iit.uni-miskolc.hu>
2016-07-21 08:40:00 +0200

    librarian - SQL scripts have been modified, book, bookInstance and bookAuthor tables have been created.

--- librarian/workspace/librarian/librarian-persist-mysql/src/main/resources/createTables.sql
+++ librarian/workspace/librarian/librarian-persist-mysql/src/main/resources/createTables.sql
@@ -6,3 +6,32 @@ birthDate date
 )
 engine=MyISAM;
 
+create table if not exists book(
+bookId integer auto_increment primary key,
+title varchar(50) not null
+)
+engine=MyISAM;
+
+create table if not exists genre(
+genre varchar(20),
+book integer,
+foreign key (book) references book(bookId) on delete cascade
+)
+engine=MyISAM;
+
+create table if not exists bookAuthor(
+bookId integer,
+authorId integer,
+primary key (bookId,authorId),
+foreign key (bookId) references book(bookId) on delete cascade,
+foreign key (authorId) references author(authorId) on delete cascade
+)
+engine=MyISAM;
+
+create table if not exists bookInstance(
+inventoryNo integer auto_increment primary key,
+instanceOfBook integer,
+price double,
+foreign key (instanceOfBook) references book(bookId) on delete cascade
+)
+engine=MyISAM;
--- librarian/workspace/librarian/librarian-persist-mysql/src/main/resources/populateDB.sql
+++ librarian/workspace/librarian/librarian-persist-mysql/src/main/resources/populateDB.sql
@@ -2,4 +2,10 @@ START TRANSACTION;
 insert into author(authorName, nationality, birthDate) values('Jozsef Attila','Hungarian',Date('1892-12-25'));
 insert into author(authorName, nationality, birthDate) values('William Shakespeare','British',Date('1392-01-15'));
 insert into author(authorName, nationality, birthDate) values('Ady Endre','Hungarian',Date('1912-05-05'));
+
+
+insert into book(title) values ('Romeo and Juliet');
+
+insert into genre values('Romance',1);
+
 COMMIT;
